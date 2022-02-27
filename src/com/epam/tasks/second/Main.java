70830be6583e4df64ee55f180031eb238785e310
MrButcher69 <90145243+MrButcher69@users.noreply.github.com>
2022-02-27 19:52:19 +0300

    Task3
    task3

---
+++ src/com/epam/tasks/second/Main.java
@@ -0,0 +1,10 @@
+package com.epam.tasks.second;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SystemLogic systemLogic = new SystemLogic();
+    }
+}
